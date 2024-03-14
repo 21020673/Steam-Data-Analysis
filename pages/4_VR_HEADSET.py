1285abe85c14d3d51136f2d879b29abf5a1f9049
NTKdz <nguyenthekhoi2003@gmail.com>
2024-03-14 08:22:18 +0700

    add vr-headsets

---
+++ pages/4_VR_HEADSET.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+
+import warnings
+warnings.filterwarnings('ignore')
+
+st.title('VR HEADSET DATA')
+
+
+def categorize_vr_headset(vr_headset):
+    if 'Windows Mixed Reality' in vr_headset:
+        return 'WINDOWS MIXED REALITY'
+    elif 'HTC' in vr_headset:
+        return 'HTC'
+    # elif 'Oculus Rift S' in vr_headset:
+    #     return 'OCULUS RIFT S'
+    elif 'Meta Quest' in vr_headset:
+        return 'META QUEST'
+    elif 'Valve Index' in vr_headset:
+        return 'VALVE INDEX'
+    elif 'Oculus' in vr_headset:
+        return 'OCULUS'
+    else:
+        return 'OTHER'
+
+
+steamdataset = pd.read_csv('shs.csv')
+
+# Display percentage of each headset type over the years
+st.write('The graph below shows the percentage of each headset type over the years.')
+
+# Filter data for VR Headsets
+vr = steamdataset.loc[(steamdataset['category'] == 'VR Headsets') & (
+    steamdataset['date'] >= '2020-03-01') & (
+    steamdataset['date'] != '2022-08-01')]
+
+# Apply categorization function to 'name' column
+vr['name'] = vr['name'].apply(categorize_vr_headset)
+
+# Group by date and name, and calculate the sum of percentages for each combination
+vr_grouped = vr.groupby(['date', 'name']).agg(
+    {'percentage': 'sum'}).reset_index()
+
+# Pivot the data to have 'date' as index and 'name' as columns, with 'percentage' as values
+vr_pivot = vr_grouped.pivot(
+    index='date', columns='name', values='percentage').fillna(0)
+
+# Plot the data using a bar chart
+st.bar_chart(vr_pivot, use_container_width=True)
+
